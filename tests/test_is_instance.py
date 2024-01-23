907555b111d5d80e10b9fb037af910aff422218d
Michael I Chen <Michael.Chen@aicadium.ai>
2024-01-23 15:45:29 -0800

    Revise collection tests
    This commit should result in several failing tests that reveal bugs.
    
    Consistency in test cases was improved and most tests are now structured
    as follows where applicable:
      1. Test positive case with subscripting
      2. Test positive case with nested subscripting
      3. Test negative case with correct outer type but wrong inner type
      4. Test negative case with wrong outer type
    
    In addition, I chose simpler test values with empty collections so the
    tests may be read and understood faster. The new values more directly
    test the key properties of the collections and I tried to reuse literals
    so the tests could be read and understood faster. Moreover, I adjusted
    the test values to cover more edge cases.
    
    For instance, when the object...
    
    - has a `len` of 0;
    - is a valid instance of the type it is checked against;
    - is of a type that itself is not subscriptable;
    - is of a type that is a subclass of a type that is subscriptable;
    
    ...and it is checked against a type that is subscriptable, a bug may be
    observed.
    
    Maybe there is a simpler way of defining this, but it's hard to think of
    examples where the object is not `''`.

--- tests/test_is_instance.py
+++ tests/test_is_instance.py
@@ -48,6 +48,7 @@ def test_slang():
     d1 = {'age': 88, 'old': True}
     d2 = {'age': 22, 'old': False}
     assert is_instance(['spam', 'and', 'eggs'], [str])
+    assert is_instance(None, None)
     assert is_instance([], [int])
     assert is_instance({1, 2, 3}, {int})
     assert is_instance({'bird': True, 'alive': False}, {str: bool})
@@ -58,36 +59,53 @@ def test_slang():
     assert not is_instance([d1, d2], [{str: str}])
 
 def test_collection():
-    assert is_instance(['cake'], Collection[str])
-    assert not is_instance(['cake'], Collection[int])
+    assert is_instance('', Collection[str])
+    assert is_instance('', Collection[Collection[str]])
+    assert not is_instance('', Collection[int])
+    assert not is_instance(0, Collection)
 
 def test_container():
-    assert is_instance(['cake'], Container[str])
-    assert not is_instance(['cake'], Container[int])
+    assert is_instance('', Container[str])
+    assert is_instance('', Container[Container[str]])
+    assert not is_instance('', Container[int])
+    assert not is_instance(0, Container)
 
 def test_iterable():
-    assert is_instance(['cake'], Iterable[str])
-    assert not is_instance(['cake'], Iterable[int])
+    assert is_instance('', Iterable[str])
+    assert is_instance('', Iterable[Iterable[str]])
+    assert not is_instance('', Iterable[int])
+    assert not is_instance(0, Iterable)
 
 def test_iterator():
-    assert is_instance(iter(['cake']), Iterator[str])
-    assert not is_instance(iter(['cake']), Iterator[int])
+    assert is_instance(iter(''), Iterator[str])
+    assert not is_instance(iter(''), Iterator[int])
+    assert not is_instance('', Iterator)
 
 def test_mapping():
-    assert is_instance({'cake': 'pie'}, Mapping[str, str])
-    assert not is_instance({'cake': 'pie'}, Mapping[str, int])
+    assert is_instance({'': ''}, Mapping[str, str])
+    assert not is_instance({'': ''}, Mapping[str, int])
+    assert not is_instance('', Mapping)
 
 def test_reversible():
-    assert is_instance(['cake'], Reversible[str])
-    assert not is_instance(['cake'], Reversible[int])
+    assert is_instance('', Reversible[str])
+    assert is_instance('', Reversible[Reversible[str]])
+    assert not is_instance('', Reversible[int])
+    assert not is_instance(set(), Reversible)
 
 def test_sequence():
-    assert is_instance(['cake'], Sequence[str])
-    assert not is_instance(['cake'], Sequence[int])
+    assert is_instance('', Sequence[str])
+    assert is_instance('', Sequence[Sequence[str]])
+    assert not is_instance('', Sequence[int])
+    assert not is_instance(set(), Sequence)
 
 ############
 ### TODO ###
 ############
+
+def TODO_test_typed_tuples_ellipsis():
+    assert is_instance((), tuple[int, ...])
+    assert is_instance((1,), tuple[int, ...])
+    assert is_instance((1, 2), tuple[int, ...])
 
 def TODO_test_callable():
     assert not is_instance(lambda: None, Callable[[str], None])
@@ -96,15 +114,10 @@ def TODO_test_callable():
     assert is_instance(fun, Callable[[str], None])
     def fun(x: str, y: int) -> None: ...
     assert is_instance(fun, Callable[[str, int], None])
-    def fun(x: str, y: int) -> bool: ...
-    assert is_instance(fun, Callable[[str, int], bool])
-
-def TODO_test_typed_tuples_ellipsis():
-    assert is_instance((), tuple[int, ...])
-    assert is_instance((1,), tuple[int, ...])
-    assert is_instance((1, 2), tuple[int, ...])
+    def fun(x: None) -> str: ...
+    assert is_instance(fun, Callable[[None], str])
 
 def TODO_test_generator():
-    assert is_instance((_ for _ in '__'), Generator[str, None, None])
-    assert not is_instance((_ for _ in '__'), Generator[int, None, None])
+    assert is_instance((_ for _ in ''), Generator[str, None, None])
+    assert not is_instance((_ for _ in ''), Generator[int, None, None])
     # TODO: test Generator[...] + send/receive
