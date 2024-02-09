984ab9e22153b225f624f49e87ecbad5e3f44c5e
Jason Wilkes <notarealdeveloper@gmail.com>
2024-02-09 15:40:53 -0800

    Fix import

--- src/is_instance/main.py
+++ src/is_instance/main.py
@@ -5,7 +5,7 @@ __all__ = [
 import sys
 import types
 import typing
-from collections.abc import Container, Generator, Iterable, Iterator, Mapping
+from collections.abc import Container, Iterable, Mapping, Sequence
 from functools import reduce
 from operator import or_
 
